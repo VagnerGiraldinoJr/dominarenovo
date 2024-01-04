cb35c4b2d3af69923bd4aaf17185ff9182a33175
GiraldinoJr <giraldinojr@gmail.com>
2024-01-03 23:34:49 -0300

    Versão final 2

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return redirect('/admin');
+    return redirect('/admin'); //melhor commit
 });
 
 Route::group(['prefix' => 'admin','middleware' => ['auth','acl'],'namespace' => 'Admin'],function(){
